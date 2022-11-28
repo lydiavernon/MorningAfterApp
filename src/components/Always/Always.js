ac2c937a18b74f07c030c140ed14c0a3ee13aa8b
lydiavernon <111576826+lydiavernon@users.noreply.github.com>
2022-11-28 18:30:39 +0000

    add favicon

--- src/components/Always/Always.js
+++ src/components/Always/Always.js
@@ -3,10 +3,6 @@ import "./Always.scss";
 import always1 from "../../assets/events/always/always1.jpg";
 import always4 from "../../assets/events/always/always4.jpg";
 
-// import always2 from "../../assets/events/always/always2.PNG";
-
-// import always3 from "../../assets/events/always/always3.mp4";
-
 const Always = () => {
   return (
     <div className="always">
